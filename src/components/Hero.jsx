ee34825bf1fd2df5389a12fa0e3c092fc0f71866
mathieu <mat.pic@outlook.fr>
2025-11-30 02:45:29 +0100

    Implement Cocktails Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,8 @@ import { useMediaQuery } from "react-responsive";
 const Hero = () => {
     const videoRef = useRef();
  
- const isMobile = useMediaQuery({ maxWidth: 767 });
+    const isMobile = useMediaQuery({ maxWidth: 767 });
+    
     useGSAP(() => {
         const heroSplit = new SplitText(".title", { type: "chars, words" })
         const paragraphSplit = new SplitText(".subtitle", { type: "lines" })
@@ -43,6 +44,25 @@ const Hero = () => {
 	    .to(".right-leaf", { y: 200 }, 0)
 	    .to(".left-leaf", { y: -200 }, 0)
 	    .to(".arrow", { y: 100 }, 0);
+
+        const startValue = isMobile ? "top 50%" : "center 60%";
+        const endValue = isMobile ? "120% top" : "bottom top";
+        
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: "video",
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            },
+        });
+        
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration,
+            });
+        };
     }, [])
     
     return (
